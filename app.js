4818d439abf94d2ab0de1f59c39750c706ebe232
do.huy.thanh <SUN-ASTERISK\do.huy.thanh@b122315-pc.sun-asterisk.com>
2020-10-31 07:45:17 +0700

    v3

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const axios = require("axios");
-const freecleverbot = require("cleverbot-free");
+// const freecleverbot = require("cleverbot-free");
+// const simsimi = require('simsimi')({
+//   key: 'ObCoOeZsgffItWF7nr-euX1VhtW4ve-8R9.61-iS',
+// });
 
 app.get("/", function (req, res) {
   res.send("Hello World csdsfss");
@@ -12,6 +15,8 @@ app.post("/mention", express.json(), async function (req, res) {
   let room_id = req.body.webhook_event.room_id;
   let from_acc = req.body.webhook_event.from_account_id;
   let mss_id = req.body.webhook_event.message_id;
+  let a = req.body.webhook_event.body.split("\n");
+
   // console.log(req.body)
   sender_info = await axios
     .get(`https://api.chatwork.com/v2/rooms/${room_id}/messages/${mss_id}`, {
@@ -20,12 +25,26 @@ app.post("/mention", express.json(), async function (req, res) {
       },
     })
     .catch((err) => console.log(err));
-  let a = req.body.webhook_event.body.split("\n");
-  sender_info2 = await freecleverbot(a[1]);
+  
+  sender_info2 = await axios
+  .post(
+    `https://wsapi.simsimi.com/190410/talk`,
+    {
+      utext: `${a[1]}`,
+      lang: "vi"
+  },
+    {
+      headers: {
+        "x-api-key": "yHNt28MJaiA132cn7uGn9ozhKIfRtE3Nh_LtyhAy",
+        'Content-Type': 'application/json'
+      },
+    }
+  )
+  .catch((err) => console.log(err));
 
-  axios
+  await axios
     .post(
-      `https://api.chatwork.com/v2/rooms/${room_id}/messages?body=[rp aid=${from_acc} to=${room_id}-${mss_id}] ${sender_info.data.account.name}\n ${sender_info2}`,
+      encodeURI(`https://api.chatwork.com/v2/rooms/${room_id}/messages?body=[rp aid=${from_acc} to=${room_id}-${mss_id}]${sender_info.data.account.name}\n${sender_info2.data.atext}`),
       {},
       {
         headers: {
